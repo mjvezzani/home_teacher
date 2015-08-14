d0185e2e01289c046f63b7c5c77b56b549619d43
Michael Vezzani <mjvezzani@gmail.com>
2015-08-14 11:27:12 -0400

    Add some tests

--- app/models/home_teacher.rb
+++ app/models/home_teacher.rb
@@ -6,4 +6,24 @@ class HomeTeacher < Member
   def full_name
     "#{firstname} #{lastname}"
   end
+
+  def set_companion(new_companion)
+    # first, nullify current companionship
+    self.companion.companion_id = ""
+    self.companion.save!
+    self.companion_id = ""
+    self.save!
+
+    # next, nullify new companion current companionship
+    new_companion.companion.companion_id = ""
+    new_companion.companion.save!
+    new_companion.companion_id = ""
+    new_companion.save!
+
+    # last, link self and new companion together
+    self.companion_id = new_companion.id
+    self.save!
+    new_companion.companion_id = self.id
+    new_companion.save!
+  end
 end
